a473121ffbf996bd4e5de7a651392b0fd83a213f
Branislav Milan Jankovic <Branislav@Branislavs-MacBook-Air.local>
2021-06-18 21:18:08 +0200

    AuthPage

--- src/pages/AuthPage.java
+++ src/pages/AuthPage.java
@@ -1,5 +1,29 @@
 package pages;
 
-public class AuthPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class AuthPage extends BasicPage{
+
+	public AuthPage(WebDriver driver, JavascriptExecutor js) {
+		super(driver, js);
+	}
+
+	public WebElement getUser() {
+		
+		return driver.findElement(By.xpath("//li[@class=\"filled \"]/a"));
+	}
+
+	
+	public WebElement getMyAccount() {
+		
+		return driver.findElement(By.linkText("My Account"));
+	}
+	
+	public WebElement getLogout() {
+		
+		return driver.findElement(By.linkText("Logout"));
+	}
 }
