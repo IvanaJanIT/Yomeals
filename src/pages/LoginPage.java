26570e21098bd50b0317e2d23b1900b5e0010654
Branislav Milan Jankovic <Branislav@Branislavs-MacBook-Air.local>
2021-06-18 17:31:56 +0200

    LoginPage

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,5 +1,52 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class LoginPage extends BasicPage{
+
+	public LoginPage(WebDriver driver) {
+		super(driver);
+	}
+
+	public WebElement getUsername() {
+		
+		return driver.findElement(By.name("username"));
+	}
+	
+	public WebElement getPassword() {
+		
+		return driver.findElement(By.name("password"));
+	}
+	
+	public WebElement getRememberMe() {
+		
+		return driver.findElement(By.name("remember_me"));
+	}
+	
+	public WebElement getLoginBtn() {
+		
+		return driver.findElement(By.name("btn_submit"));
+	}
+	
+	public WebElement getForgotPassword() {
+		
+		return driver.findElement(By.linkText("Password"));
+	}
+	
+	public WebElement getFacebookLogin() {
+		
+		return driver.findElement(By.xpath("//a[text()='Facebook']"));
+	}
+	
+	public WebElement getGoogleLogin() {
+		
+		return driver.findElement(By.xpath("//a[text()='Google']"));
+	}
+	
+	public WebElement getSignUpNow() {
+		
+		return driver.findElement(By.linkText("Signup Now"));
+	}
 }
