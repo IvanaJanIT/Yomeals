058c9d97d6a5f6d875ee60e734d3b899e13ea730
Branislav Milan Jankovic <Branislav@Branislavs-MacBook-Air.local>
2021-06-19 22:18:42 +0200

    CartSummaryPage

--- src/pages/CartSummaryPage.java
+++ src/pages/CartSummaryPage.java
@@ -1,5 +1,23 @@
 package pages;
 
-public class CartSummaryPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class CartSummaryPage extends BasicPage {
+
+	public CartSummaryPage(WebDriver driver, JavascriptExecutor js) {
+		super(driver, js);
+	}
+
+	public WebElement getClearAll() {
+
+		return driver.findElement(By.linkText("Clear All"));
+	}
+
+	public void clearAllFromCart() {
+
+		this.getClearAll().click();
+	}
 }
